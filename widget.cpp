139b4cefd1fc5787bd55b8caf9983886dd623e85
pierrbt <pierre08bt@gmx.co.uk>
2022-07-21 17:03:33 +0200

    Update widget.cpp
    I fixed the timestamp correction

--- widget.cpp
+++ widget.cpp
@@ -266,30 +266,50 @@ QStringList findSimilarAirport(QString nameOfTheCity)
 
 QString Window::correctTS(QString input)
 {
-  input = input.remove(input.size()-4, 4);
-
+      // Récupération des valeurs dans le timestamp
+      input = input.remove(input.size()-4, 4);
       QString year = input.sliced(0, 4);
-
       QString month = input.sliced(5,2);
       QString day = input.sliced(8,2);
-
-
-
       input = input.remove(0, 11);
-      QString time = QString::number(((input.sliced(0,2).toInt()+2) > 24 ? input.sliced(0,2).toInt()+2-24 : input.sliced(0,2).toInt()+2)) + input.sliced(2,input.size()-2);
-      qDebug() << year << month << day << time;
+      int hour = input.sliced(0,2).toInt();
+      int min = input.sliced(3,2).toInt();
+
+      bool yesterday = false;
+
+      //Création de la date
       QDate *messDate = new QDate;
       messDate->setDate(year.toInt(), month.toInt(), day.toInt());
+
+
+        // On regarde si la date est celle d'aujourd'hui ou d'hier
       if(messDate->toString() == QDate::currentDate().toString())
-          input = "Aujourd'hui à " + time;
+          input = "Aujourd'hui à ";
       else if((messDate->year() == QDate::currentDate().year())   &&
               (messDate->month() == QDate::currentDate().month()) &&
               (messDate->day() == ((QDate::currentDate().day())-1)))
-                  input = "Hier à " + time;
-      else
-           input = "Le " + day + "/" + month + "/" + year + " à " + time;
+      {
+                  input = "Hier à ";
+                  yesterday = true;
+      }
 
-      return input;
+      else
+           input = "Le " + day + "/" + month + "/" + year + " à ";
+
+      // Si on obtient une heure GMT+2 >= 24 heures, on enlève 22 pour retrouver la vraie heure
+      if(hour + 2 >= 24)
+       {
+           hour-=22;
+           if(yesterday)
+               input = "Aujourd'hui à ";
+       }
+      else // Sinon on ajoute 2 heures pour avoir l'heure française et pas GMT
+      {
+          hour+=2;
+      }
+      // Commme c'est des nombres, ça s'affiche 1 si il est une heure alors qu'on veut 01h, on ajoute donc un 0 si c'est < à 10
+       QString time = ((hour < 10) ? ("0"+QString::number(hour)) : (QString::number(hour))) + ":" + ((min < 10) ? ("0" + QString::number(min)) : (QString::number(min)));
+      return input + time;
 }
 
 Window::Window(QString val)
